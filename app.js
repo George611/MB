0703bd4562d3f29d732725167009acf7b251483a
George <George@hotmail.com>
2023-12-07 12:19:11 +0200

    2nd

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/',function(req,res){
-    res.send ('hello World')
-
+    res.send ('Mabelle')
 })
+app.get('/Food',function(req,res){
+    var data = [
+        {id:1, name:'Tawouk'},
+        {id:2, name:'Burger'},
+        {id:3, name:'Fries'},
+        {id:4, name:'Crispy'},
+    ]
+    res.send (data)
+})
+
 app.listen(PORT)
